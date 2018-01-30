4ef33d240f3f64c03fd52989694289169530ce42
David Niquette <niquetted@wit.edu>
2018-01-29 19:05:44 -0500

    template parsing

--- frontend/collector_view.py
+++ frontend/collector_view.py
@@ -5,11 +5,21 @@ clr.AddReferenceByName("PresentationCore, Version=3.0.0.0, Culture=neutral, Publ
 clr.AddReference('WindowsBase')
 import System.Windows
 from System.Windows.Threading import DispatcherTimer as Timer
+from System.Windows.Markup import XamlReader
+from System.IO import File
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 Window = System.Windows.Window
 StackPanel = System.Windows.Controls.StackPanel
 Label = System.Windows.Controls.Label
+ComboBox = System.Windows.Controls.ComboBox
+TextBox = System.Windows.Controls.TextBox
 Button = System.Windows.Controls.Button
+TabItem = System.Windows.Controls.TabItem
+TabControl = System.Windows.Controls.TabControl
 
 class CollectorView(Window):
 
@@ -22,10 +32,57 @@ class CollectorView(Window):
         self.value_label.Content = 'Test'
         self.stack.Children.Add(self.value_label)
 
+        self.curr_box = ComboBox()
+        self.stack.Children.Add(self.curr_box)
+
+        self.gain_box = TextBox()
+        self.stack.Children.Add(self.gain_box)
+
         timer = Timer()
         timer.Tick += self.update_view
         timer.Interval = System.TimeSpan.FromSeconds(1)
         timer.Start()
 
+    def load_ui_from_xaml(self):
+        self.mainwin = XamlReader.Load(self.ui_file)
+
     def update_view(self, sender, args):
         self.value_label.Content = System.DateTime.Now.ToLongTimeString()
+
+class CollectorXAMLView():
+    """
+    Content is always a grid
+    """
+    def __init__(self, ui_file):
+        self.ui_file = File.OpenRead(ui_file)
+        self.window = XamlReader.Load(self.ui_file)
+        # Window.Grid.TabControl.Tabs
+        self.window_tabs = self.window.Content.Children[0].Items
+        self.tabs = {}
+        self.store_elements_in_dict()
+
+    def get_tab_children(self, tab_item):
+        return tab_item.Content.Children[0]
+
+    def get_grid_children(self, grid_item):
+        return grid_item.Children
+
+    def store_elements_in_dict(self):
+        for tab in self.window_tabs:
+            self.tabs[tab.Name] = self.get_tab_children_as_dict(tab)
+
+    def get_tab_children_as_dict(self, tab_item):
+        log.info("##################")
+        log.info("{} {}".format(type(tab_item),tab_item.Name))
+        tab_children = {}
+        # tab.Grid.Children
+        if tab_item.Content.Children.Count > 0:
+            #for child in tab.grid.children
+            for child in tab_item.Content.Children:
+                if type(child) is TabControl:
+                    # For tab in tab control
+                    for child_item in child.Items:
+                        if type(child_item) is TabItem:
+                            self.get_tab_children_as_dict(child_item)
+                else:
+                    log.info(child.Name)
--- frontend/main.py
+++ frontend/main.py
@@ -4,12 +4,13 @@ clr.AddReferenceByName("PresentationFramework, Version=3.0.0.0, Culture=neutral,
 clr.AddReferenceByName("PresentationCore, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
 import System.Windows
 
-from frontend.collector_view import CollectorView
+from frontend.collector_view import CollectorView, CollectorXAMLView
 
 # Initialization Constants
 Application = System.Windows.Application
 
 # Run application
+xaml_view = CollectorXAMLView('E:\Dev\CoinCollectorTemplate\CoinCollectorTemplate\MainWindow.xaml')
 view = CollectorView()
 app = Application()
-app.Run(view)
+app.Run(xaml_view.window)
